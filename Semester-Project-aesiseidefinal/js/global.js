bf5ea9ea1cb9cd8b3dd8b287e38574a7295490f3
Aesiseide <91593840+Aesiseide@users.noreply.github.com>
2022-05-30 22:27:51 +0200

    Delete Semester-Project-aesiseidefinal directory

--- Semester-Project-aesiseidefinal/js/global.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector(".menu");
-let header = document.querySelector("header");
-//hide or show the menu after the menu button is clicked
-menu.addEventListener("click", () => {
-  header.classList.toggle("active");
-});
